6dc49749da2ea2010cfee73b51e25bd145520643
eileenpeacock <ejpeacock@hotmail.com>
2019-02-24 12:26:19 +0000

    Commit Json file & updated Region Pie chart

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,25 +1,10 @@
-/*d3.csv("data/international_visitors_london.csv", function (err, data) {
-    if (err) throw err;
-    
-    var ndx = crossfilter(data);
-    var all = ndx.groupAll();
-}); */
 
 queue()
-    .defer(d3.csv, 'data/international_visitors_london.csv')
+    .defer(d3.json, 'data/international_visitors_london.json')
     .await(makeGraphs);
 
 function makeGraphs(error, visitorData) {
     var ndx = crossfilter(visitorData);
-
-
-   /*Change strings to float & integer values*/ 
-    visitorData.forEach(function (d) {
-        d.visits = parseFloat(d.visits);
-        d.spend = parseFloat(d.spend);
-        d.nights = parseFloat(d.nights);
-        d.sample = parseInt(d.sample);
-    });
   
     /*Call each chart function */
     show_total_visits_per_region(ndx);
@@ -28,16 +13,18 @@ function makeGraphs(error, visitorData) {
 
 }
 
+/*Pie Chart showing Region splits */
 function show_total_visits_per_region (ndx){
 
-    var name_dim = ndx.dimension(dc.pluck('Region'));
+    var name_dim = ndx.dimension(dc.pluck('region'));
     var region_group = name_dim.group().reduceSum(dc.pluck('visits'));
     
     dc.pieChart('#region_totals_chart')
-        .height(300)
-        .radius(50)
+        .height(500)
+        .radius(100)
         .transitionDuration(1500)
         .dimension(name_dim)
         .group(region_group);
         
 }
+
