45897bc2e0af5460cfc5ba0b48b74d3572a27ca4
eileenpeacock <ejpeacock@hotmail.com>
2019-02-24 21:22:45 +0000

    'Add drop down year selector & Row chart for spend per market'

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,4 +1,3 @@
-
 queue()
     .defer(d3.json, 'data/international_visitors_london.json')
     .await(makeGraphs);
@@ -7,10 +6,25 @@ function makeGraphs(error, visitorData) {
     var ndx = crossfilter(visitorData);
   
     /*Call each chart function */
+    show_year_selector (ndx)
     show_total_visits_per_region(ndx);
+    show_top_spend_per_market (ndx)
 
     dc.renderAll();
 
+}
+
+/* Dropdown menu for year selector */
+
+function show_year_selector(ndx) {
+    var year_dim = ndx.dimension(dc.pluck('year'));
+    var year_selector = year_dim.group();
+
+    dc.selectMenu("#year_select")
+        .dimension(year_dim)
+        .group(year_selector)
+        .promptText('All Years');
+        
 }
 
 /*Pie Chart showing Region splits */
@@ -28,3 +42,22 @@ function show_total_visits_per_region (ndx){
         
 }
 
+/* Row chart of top 10 visitors */
+
+function show_top_spend_per_market (ndx) {
+     
+    var market_spend_dim = ndx.dimension(dc.pluck('market'));
+    var spend_group = market_spend_dim.group().reduceSum(dc.pluck('spend'))
+    
+
+    dc.rowChart('#top10Spend')
+        .height(300)
+        .width(400)
+        .dimension(market_spend_dim)
+        .group(spend_group)
+        .gap(5)
+        .data(function (group) { return group.top(10); })
+        .elasticX(true)
+        .xAxis().ticks(5);
+  
+}
