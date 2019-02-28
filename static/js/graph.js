da6e1ca05c267f38dc2d1625b881e6aa738d3ede
eileenpeacock <ejpeacock@hotmail.com>
2019-02-28 21:40:40 +0000

    Add stacked bar chart

--- static/js/graph.js
+++ static/js/graph.js
@@ -9,6 +9,10 @@ function makeGraphs(error, visitorData) {
     show_year_selector (ndx)
     show_total_visits_per_region(ndx);
     show_top_spend_per_market (ndx)
+    show_mode_of_travel (ndx)
+    show_purpose_of_travel (ndx)
+    // show_average_spend_per_duration (ndx)
+    // show_spend_years_qtrs (ndx)
 
     dc.renderAll();
 
@@ -61,3 +65,86 @@ function show_top_spend_per_market (ndx) {
         .xAxis().ticks(5);
   
 }
+
+/*Pie Chart showing Mode of travel splits */
+function show_mode_of_travel (ndx){
+
+    var mode_dim = ndx.dimension(dc.pluck('mode'));
+    var mode_travel_group = mode_dim.group();
+    
+    dc.pieChart('#mode_travel')
+        .height(500)
+        .radius(100)
+        .transitionDuration(1500)
+        .dimension(mode_dim)
+        .group(mode_travel_group);
+}
+
+/*Stacked bar chart for purpose of travel & spend */
+function show_purpose_of_travel (ndx){
+
+    var purpose_dim = ndx.dimension(dc.pluck('dur_stay'));
+
+    var spendByPurpose_business = purpose_dim.group().reduceSum( function (d){
+        if (d.purpose === 'Business') {
+            return +d.spend; 
+        } else {
+            return 0;
+        }
+    });
+
+    var spendByPurpose_holiday = purpose_dim.group().reduceSum( function (d){
+        if (d.purpose === 'Holiday') {
+            return +d.spend; 
+        } else {
+            return 0;
+        }
+    });
+
+    var spendByPurpose_vfr = purpose_dim.group().reduceSum( function (d){
+        if (d.purpose === 'VFR') {
+            return +d.spend; 
+        } else {
+            return 0;
+        }
+    });
+
+      var spendByPurpose_misc = purpose_dim.group().reduceSum( function (d){
+        if (d.purpose === 'Miscellaneous') {
+            return +d.spend ; 
+        } else {
+            return 0;
+        }
+    });
+
+    var spendByPurpose_study = purpose_dim.group().reduceSum( function (d){
+        if (d.purpose === 'Study') {
+            return +d.spend; 
+        } else {
+            return 0;
+        }
+    });
+
+    var stackedChart = dc.barChart('#stacked_purpose_travel');
+    stackedChart    
+        .width(500)
+        .height(500)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(purpose_dim)
+        .group(spendByPurpose_business, 'Business')
+        .stack(spendByPurpose_holiday, 'Holiday')
+        .stack(spendByPurpose_vfr, 'Vising Friends & Relatives')
+        .stack(spendByPurpose_misc, 'Miscellaneous')
+        .stack(spendByPurpose_study, 'Study')
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticX(true)
+        .legend(dc.legend().x(480).y(0).itemHeight(10).gap(5));
+
+    
+}
+
+
+
+
+
